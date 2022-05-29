cbe632681d6b2eef70df26fa0a9931529354395a
nataekoon <smathj@naver.com>
2022-05-29 11:16:27 +0900

    non blocking, call back pattern

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const app = express();
 
 const users = [];
 
-// request body데이터의 json데이터를 가져오기위해서
+// request body데이터의 json데이터를 가져오기위해서 ( 옛날의 bodyParser임 )
 app.use(express.json());
+
+app.post('/user', (req, res) => {
+  let user = req.body;
+  users.push({ user });
+  console.log(user);
+  return res.send({ success: true });
+});
 
 app.get('/user', (req, res) => {
   return res.send({ users });
-});
-
-app.post('/user', (req, res) => {
-  users.push({ name: req.body.name, age: req.body.age });
-  return res.send({ success: true });
 });
 
 app.listen(3000, () => {
